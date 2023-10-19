726d8edba7fb8ffba2bc912e29085cbbc71c131e
kovan <you@example.com>
2023-10-19 04:30:38 +0200

    Update .config/omf/before.init.fish

--- dot_config/omf/before.init.fish
+++ dot_config/omf/before.init.fish
@@ -2,6 +2,7 @@ alias lola "git log --graph --decorate --pretty=oneline --abbrev-commit"
 alias drop_cache "sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
 alias vi vim
 alias mapkeys "gsettings set org.gnome.desktop.input-sources xkb-options \"['ctrl:swap_lwin_lctl']\""
+alias sc "sudo systemctl"
 
 fish_add_path $HOME/bin
 fish_add_path $HOME/.local/bin
