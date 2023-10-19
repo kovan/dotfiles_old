4f10e7b9945cf63b3dd1554fd1b01272950e1265
kovan <you@example.com>
2023-10-19 04:20:20 +0200

    Update .config/omf/before.init.fish

--- dot_config/omf/before.init.fish
+++ dot_config/omf/before.init.fish
@@ -1,5 +1,11 @@
-fish_add_path $HOME/.poetry/bin 
+alias lola "git log --graph --decorate --pretty=oneline --abbrev-commit"
+alias drop_cache "sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
+alias vi vim
+alias mapkeys "gsettings set org.gnome.desktop.input-sources xkb-options \"['ctrl:swap_lwin_lctl']\""
+
 fish_add_path $HOME/bin
-fish_add_path /home/k/.local/bin
-fish_add_path ~/.config/emacs/bin 
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.config/emacs/bin 
+fish_add_path /snap/bin 
+fish_add_path $HOME/.poetry/bin 
 
